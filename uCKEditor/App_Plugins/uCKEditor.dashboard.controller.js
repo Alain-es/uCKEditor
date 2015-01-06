87cff2d19c8c2f732b2610c836d8965af16504f5
Alain <alain.m@outlook.es>
2015-01-06 22:13:50 +0000

    Fixed bug with dialog's fullscreen mode

--- uCKEditor/App_Plugins/uCKEditor.dashboard.controller.js
+++ uCKEditor/App_Plugins/uCKEditor.dashboard.controller.js
@@ -2,7 +2,10 @@
 (function () {
 
     //Main controller
-    function uCKEditorDashboardController($log, $rootScope, $scope, dialogService, $window) {
+    function uCKEditorDashboardController($log, $rootScope, $scope, dialogService, $window, assetsService) {
+
+        // Load the css that converts the right hand side dialog into a full screen dialog
+        assetsService.loadCss("/App_Plugins/uCKEditor/uCKEditor.dashboard.css");
 
         // This value is checked by the javascript executed when the user cicks on the button to check if the 
         // image picker is loaded
